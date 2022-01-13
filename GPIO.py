806b0292abc5178a0ab1a793d47dc0bf284839fd
Andrea M <a62marti@uwaterloo.ca>
2022-01-12 20:33:29 -0500

    Combine GPIO write functions into one

--- GPIO.py
+++ GPIO.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 
 def init():
-    GPIO.setmode(GPIO.BCM)    #use broadcom pin numbers (label on pins not physical pin number)
+    GPIO.setmode(GPIO.BCM)          #use broadcom pin numbers (label on pins not physical pin number)
+    GPIO.setwarnings(True)
 
 def setPin(pin, direction, PUD):
     if PUD == 'NONE':
@@ -9,7 +10,7 @@ def setPin(pin, direction, PUD):
     else:
         GPIO.setup(pin, setDirection(direction), pull_up_down = setPUD(PUD))
 
-def setDirection(direction):  #not meant to be used outside this module
+def setDirection(direction):        #not meant to be used outside this module
     if direction == 'OUT':
         return GPIO.OUT
     elif direction == 'IN':
@@ -18,7 +19,7 @@ def setDirection(direction):  #not meant to be used outside this module
         print ("Error in direction declaration, expected either: 'IN' or 'OUT'")
         return
 
-def setPUD(PUD):              #not meant to be used outside this module
+def setPUD(PUD):                    #not meant to be used outside this module
     if PUD == 'UP':
         return GPIO.PUD_UP
     elif PUD == 'DOWN':
@@ -27,13 +28,15 @@ def setPUD(PUD):              #not meant to be used outside this module
         print ("Error in PUD declaration, expected either: 'NONE', 'UP', or 'DOWN'")
         return
 
-def setHigh(pin):
-    GPIO.output(pin, GPIO.HIGH)
+def write(pin, output):
+    if output == 'HIGH':
+        GPIO.output(pin, GPIO.HIGH)
+    elif output == 'LOW':
+        GPIO.output(pin, GPIO.LOW)
+    else:
+        GPIO.output(pin, output)    #values accepted are 1, 0, True, False - RPi.GPIO will send an error if it isn't
 
-def setLow(pin):
-    GPIO.output(pin, GPIO.LOW)
-
-def read(pin):
+def read(pin):                      #returns data read at pin - 1 or 0
     return GPIO.input(pin)
 
 def closeGPIO():
