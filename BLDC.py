ae6da771957764784f6fbb4840b5647db8d86de6
Andrea M <a62marti@uwaterloo.ca>
2022-01-21 11:31:23 -0500

    Update BLDC.py error messages

--- BLDC.py
+++ BLDC.py
@@ -12,12 +12,11 @@ class motor:
 
     def setFrequency(motor, frequency):
         if frequency < 15000:
-            print ("Error: frequency is too low, please set it to a value between 15000 and 25000")
+            raise ValueError("Frequency is too low, please set it to a value between 15000 and 25000")
         elif frequency > 25000:
-            print ("Error: frequency is too high, please set it to a value between 15000 and 25000")
+            raise ValueError("Frequency is too high, please set it to a value between 15000 and 25000")
         else:
             pwmCtrl.setPin(motor.pwmPin, frequency)
-            print ("Successfully set frequency")
 
     def setDirection(motor, direction):
         if direction == 'CW':
@@ -25,11 +24,11 @@ class motor:
         elif direction == 'CCW':
             GPIO.write(motor.directionPin, 'HIGH')
         else:
-            print ("Could not set motor direction, expected either: 'CW' or 'CCW'")
+            raise ValueError("Could not set motor direction, expected either: 'CW' or 'CCW'")
 
     def setSpeed(motor, speed):                     #speed in percentage
         if speed < 50:
-            print ("Error: speed is too low, please set it to a value higher than 50%")
+            raise ValueError("Speed is too low, please set it to a value higher than 50%")
         else:
             motor.speed = speed
 
