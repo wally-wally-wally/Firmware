bca56a292e11e73c22b059b4c096ce3111fe7424
Andrea M <a62marti@uwaterloo.ca>
2022-01-24 13:20:04 -0500

    Move global variable to BLDC constructor

--- BLDC.py
+++ BLDC.py
@@ -1,39 +1,38 @@
 import GPIO
 import PWM
 
-pwmCtrl = PWM.signal()
+class Motor:
+    def __init__(self, pwmPin, directionPin):
+        self.pwmCtrl = PWM.Signal()
+        self.pwmPin = pwmPin
+        self.directionPin = directionPin
+        self.speed = 0
+        GPIO.setPin(self.directionPin, 'OUT', 'NONE')
 
-class motor:
-    def __init__(motor, pwmPin, directionPin):
-        motor.pwmPin = pwmPin
-        motor.directionPin = directionPin
-        motor.speed = 0
-        GPIO.setPin(motor.directionPin, 'OUT', 'NONE')
-
-    def setFrequency(motor, frequency):
+    def setFrequency(self, frequency):
         if frequency < 15000:
             raise ValueError("Frequency is too low, please set it to a value between 15000 and 25000")
         elif frequency > 25000:
             raise ValueError("Frequency is too high, please set it to a value between 15000 and 25000")
         else:
-            pwmCtrl.setPin(motor.pwmPin, frequency)
+            self.pwmCtrl.setPin(self.pwmPin, frequency)
 
-    def setDirection(motor, direction):
+    def setDirection(self, direction):
         if direction == 'CW':
-            GPIO.write(motor.directionPin, 'LOW')
+            GPIO.write(self.directionPin, 'LOW')
         elif direction == 'CCW':
-            GPIO.write(motor.directionPin, 'HIGH')
+            GPIO.write(self.directionPin, 'HIGH')
         else:
             raise ValueError("Could not set motor direction, expected either: 'CW' or 'CCW'")
 
-    def setSpeed(motor, speed):                     #speed in percentage
+    def setSpeed(self, speed):                     #speed in percentage
         if speed < 50:
             raise ValueError("Speed is too low, please set it to a value higher than 50%")
         else:
-            motor.speed = speed
+            self.speed = speed
 
-    def start(motor):
-        pwmCtrl.begin(motor.speed)
+    def start(self):
+        self.pwmCtrl.begin(self.speed)
 
-    def stop(motor):
-        pwmCtrl.stop()
+    def stop(self):
+        self.pwmCtrl.stop()
