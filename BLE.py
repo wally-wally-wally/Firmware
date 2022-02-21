44f981255676347fca87eff11957e4001f7d3e62
Andrea M <a62marti@uwaterloo.ca>
2022-02-20 19:58:30 -0500

    Change BLE.py to use 'self'

--- BLE.py
+++ BLE.py
@@ -8,25 +8,25 @@
 import bluetooth
 
 class Socket:
-    def __init__(socket):
-        socket.server = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    def __init__(self):
+        self.server = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
         port = 1
-        socket.server.bind(("",port))
+        self.server.bind(("",port))
 
-    def advertise(socket):
-        socket.server.listen(1)
+    def advertise(self):
+        self.server.listen(1)
 
-    def connect(socket):
-        socket.client, address = socket.server.accept()
+    def connect(self):
+        self.client, address = self.server.accept()
         print ("Accepted connection from", address)
 
-    def read(socket):
-        return socket.client.recv(1024) #returns data read
+    def read(self):
+        return self.client.recv(1024) #returns data read
 
-    def write(socket, data):
-        socket.client.send(data)
+    def write(self, data):
+        self.client.send(data)
 
-    def disconnect(socket):
-        socket.client.close()
-        socket.server.close()
+    def disconnect(self):
+        self.client.close()
+        self.server.close()
 
