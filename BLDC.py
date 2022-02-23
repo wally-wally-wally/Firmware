728b7d46c3a75df1cc65629ad0bbcaca58e8147c
Andrea M <a62marti@uwaterloo.ca>
2022-02-23 13:20:55 -0500

    Remove bugs from testing

--- BLDC.py
+++ BLDC.py
@@ -27,7 +27,7 @@ class Motor:
             GPIO.write(self.directionPin, 'HIGH')
         else:
             raise ValueError("Could not set motor direction, expected either: 'CW' or 'CCW'")
-        time.sleep(DELAY)
+        time.sleep(self.DELAY)
 
     def setMotorSpeed(self, speed): #speed in percentage
         self.speed = speed
@@ -36,63 +36,62 @@ class Motor:
         self.pwmCtrl.begin(self.speed)
 
     def stopMotor(self):
-        self.speed = 0
         self.pwmCtrl.stop()
-        time.sleep(DELAY)
+        time.sleep(self.DELAY)
 
 class Navigation:
     def __init__(self, pwmPinFR, pwmPinFL, pwmPinBR, pwmPinBL, dirPinFR, dirPinFL, dirPinBR, dirPinBL, frequency):
-        self.FR = Motor(pwPinFR, dirPinFR, frequency)
-        self.FL = Motor(pwPinFL, dirPinFL, frequency)
-        self.BR = Motor(pwPinBR, dirPinBR, frequency)
-        self.BL = Motor(pwPinBL, dirPinBL, frequency)
+        self.FR = Motor(pwmPinFR, dirPinFR, frequency)
+        self.FL = Motor(pwmPinFL, dirPinFL, frequency)
+        self.BR = Motor(pwmPinBR, dirPinBR, frequency)
+        self.BL = Motor(pwmPinBL, dirPinBL, frequency)
 
     def forward(self):
         self.stop()
         self.FR.setDirection('CCW') #forward
-        self.FL.setDirection('CCW') #forward
+        self.FL.setDirection('CW')  #forward
         self.BR.setDirection('CCW') #forward
-        self.BL.setDirection('CCW') #forward
+        self.BL.setDirection('CW')  #forward
         self.start()
 
     def backward(self):
         self.stop()
         self.FR.setDirection('CW')  #backward
-        self.FL.setDirection('CW')  #backward
+        self.FL.setDirection('CCW') #backward
         self.BR.setDirection('CW')  #backward
-        self.BL.setDirection('CW')  #backward
+        self.BL.setDirection('CCW') #backward
         self.start()
 
     def right(self):
         self.stop()
         self.FR.setDirection('CW')  #backward
-        self.FL.setDirection('CCW') #forward
+        self.FL.setDirection('CW')  #forward
         self.BR.setDirection('CCW') #forward
-        self.BL.setDirection('CW')  #backward
+        self.BL.setDirection('CCW') #backward
         self.start()
 
     def left(self):
         self.stop()
         self.FR.setDirection('CCW') #forward
-        self.FL.setDirection('CW')  #backward
+        self.FL.setDirection('CCW') #backward
         self.BR.setDirection('CW')  #backward
-        self.BL.setDirection('CCW') #forward
+        self.BL.setDirection('CW')  #forward
         self.start()
 
     def ccw(self):
         self.stop()
         self.FR.setDirection('CCW') #forward
-        self.FL.setDirection('CW')  #backward
+        self.FL.setDirection('CCW') #backward
         self.BR.setDirection('CCW') #forward
-        self.BL.setDirection('CW')  #backward
+        self.BL.setDirection('CCW') #backward
         self.start()
 
     def cw(self):
         self.stop()
-        self.FR.setDirection('CW')  #backward
-        self.FL.setDirection('CCW') #forward
-        self.BR.setDirection('CW')  #backward
-        self.BL.setDirection('CCW') #forward
+        self.FR.setDirection('CW') #backward
+        self.FL.setDirection('CW') #forward
+        self.BR.setDirection('CW') #backward
+        self.BL.setDirection('CW') #forward
         self.start()
     
     def setSpeed(self, speed):      #add buffers if necessary
