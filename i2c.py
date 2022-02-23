17311faf7221a6890aaf6b2da810d4cabca7d3ea
RPi <rs2thoma@uwaterloo.ca>
2022-02-23 11:38:48 -0500

    Added i2c single byte read/writes

--- i2c.py
+++ i2c.py
@@ -9,6 +9,14 @@ class I2C():
     def write(self, addr, reg, val):
         self.bus.write_block_data(addr, reg, val)
 
+    # Writes a byte directly without sending the register value; used by mux
+    def write_byte(self, addr, val)
+        self.bus.write_byte(addr, val)
+
+    # Reads a byte directly without sending the register value; used by mux
+    def read_byte(self, addr, val)
+        return self.bus.read_byte(addr, val)
+
     def read(self, addr, reg):
         return self.bus.read_block_data(addr, reg)
 
