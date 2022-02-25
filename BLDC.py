4af1d58d799bf3855ccb11730e83ca3bb1445f0d
Andrea M <33696524+amc1234@users.noreply.github.com>
2022-02-25 01:11:47 -0500

    Add Class Navigation to BLDC.py (#12)
    * Add bluetooth interface
    
    * Add GPIO interface
    
    * Add PWM interface
    
    * Create signal class
    
    * Add BLDC interface
    
    * Update BLDC.py error messages
    
    * Move global variable to BLDC constructor
    
    * Update BLDC constructor based on changes in PWM.py
    
    * Add BMS interface
    
    * Updata sleep timer
    
    * Fix bugs in BMS module
    
    * Update BLDC module from testing
    
    * Create static variable for time delay
    
    * Remove stop() when changing BLDC speed
    
    * Add BLDC test files
    
    * Add toggle function to BLDC test file
    
    * Remove bugs from testing
    
    Co-authored-by: Andrea M <a62marti@uwaterloo.ca>

--- BLDC.py
+++ BLDC.py
@@ -28,17 +28,88 @@ class Motor:
             GPIO.write(self.directionPin, 'HIGH')
         else:
             raise ValueError("Could not set motor direction, expected either: 'CW' or 'CCW'")
-        time.sleep(DELAY)
-        self.start()
+        time.sleep(self.DELAY)
 
-    def setSpeed(self, speed):                     #speed in percentage
+    def setMotorSpeed(self, speed): #speed in percentage
         self.speed = speed
-        self.start()
 
-    def start(self):
+    def startMotor(self):
         self.pwmCtrl.begin(self.speed)
 
-    def stop(self):
-        self.speed = 0
+    def stopMotor(self):
         self.pwmCtrl.stop()
-        time.sleep(DELAY)
+        time.sleep(self.DELAY)
+
+class Navigation:
+    def __init__(self, pwmPinFR, pwmPinFL, pwmPinBR, pwmPinBL, dirPinFR, dirPinFL, dirPinBR, dirPinBL, frequency):
+        self.FR = Motor(pwmPinFR, dirPinFR, frequency)
+        self.FL = Motor(pwmPinFL, dirPinFL, frequency)
+        self.BR = Motor(pwmPinBR, dirPinBR, frequency)
+        self.BL = Motor(pwmPinBL, dirPinBL, frequency)
+
+    def forward(self):
+        self.stop()
+        self.FR.setDirection('CCW') #forward
+        self.FL.setDirection('CW')  #forward
+        self.BR.setDirection('CCW') #forward
+        self.BL.setDirection('CW')  #forward
+        self.start()
+
+    def backward(self):
+        self.stop()
+        self.FR.setDirection('CW')  #backward
+        self.FL.setDirection('CCW') #backward
+        self.BR.setDirection('CW')  #backward
+        self.BL.setDirection('CCW') #backward
+        self.start()
+
+    def right(self):
+        self.stop()
+        self.FR.setDirection('CW')  #backward
+        self.FL.setDirection('CW')  #forward
+        self.BR.setDirection('CCW') #forward
+        self.BL.setDirection('CCW') #backward
+        self.start()
+
+    def left(self):
+        self.stop()
+        self.FR.setDirection('CCW') #forward
+        self.FL.setDirection('CCW') #backward
+        self.BR.setDirection('CW')  #backward
+        self.BL.setDirection('CW')  #forward
+        self.start()
+
+    def ccw(self):
+        self.stop()
+        self.FR.setDirection('CCW') #forward
+        self.FL.setDirection('CCW') #backward
+        self.BR.setDirection('CCW') #forward
+        self.BL.setDirection('CCW') #backward
+        self.start()
+
+    def cw(self):
+        self.stop()
+        self.FR.setDirection('CW') #backward
+        self.FL.setDirection('CW') #forward
+        self.BR.setDirection('CW') #backward
+        self.BL.setDirection('CW') #forward
+        self.start()
+    
+    def setSpeed(self, speed):      #add buffers if necessary
+        self.FR.setMotorSpeed(int(speed))
+        self.FL.setMotorSpeed(int(speed))
+        self.BR.setMotorSpeed(int(speed))
+        self.BL.setMotorSpeed(int(speed))
+        self.start()
+    
+    def stop(self):
+        self.FR.stopMotor()
+        self.FL.stopMotor()
+        self.BR.stopMotor()
+        self.BL.stopMotor()
+    
+    def start(self):
+        self.FR.startMotor()
+        self.FL.startMotor()
+        self.BR.startMotor()
+        self.BL.startMotor()
