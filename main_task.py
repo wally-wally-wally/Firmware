bd64f647b1c07d673ad05f6ee489d354088585b7
Andrea M <a62marti@uwaterloo.ca>
2022-03-07 00:07:44 -0500

    Add main loop

--- main_task.py
+++ main_task.py
@@ -1,7 +1,6 @@
 import path
 import BLE
 import BLDC
-import time
 
 PWM_PIN_FR =
 PWM_PIN_FL =
@@ -16,6 +15,7 @@ PWM_FREQUENCY = 20000
 def init():
     wireless = BLE.Socket()
     wally = BLDC.Navigation(PWM_PIN_FR, PWM_PIN_FL, PWM_PIN_BR, PWM_PIN_BL, DIR_PIN_FR, DIR_PIN_FL, DIR_PIN_BR, DIR_PIN_BL, PWM_FREQUENCY)
+    path = path.PathManagement(wireless, wally)
 
     wireless.advertise()
     wireless.connect()
@@ -23,9 +23,11 @@ def init():
 def mainTask():
     init()
     
-    while True:                 #BLE data received to be determined by app
+    while True:                         #BLE data received determined by app - TBD
         data = wireless.read()
         if data == 'record':
+            pathName = wireless.read()  #pathName might need to be converted depending on how we're receiving BLE data
+            path.recordPath(str(pathName))
+        elif data == 'execute':
             pathName = wireless.read()
-            
-            
+            path.executePath(str(pathName))
