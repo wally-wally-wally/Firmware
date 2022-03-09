c250e88a653984fc868b49d459f2a9367c451e41
Riley <rs2thoma@uwaterloo.ca>
2022-03-09 13:16:47 -0500

    update lidar file

--- lidar.py
+++ lidar.py
@@ -2,7 +2,7 @@ import uart
 import math
 
 DATA_HEADER_LOWER = 'aa'
-DATA_HEADER_UPPER = '5500'
+DATA_HEADER_UPPER = '55'
 
 BAUD = 115200
 PORT = uart.UART_0
@@ -17,24 +17,33 @@ class Lidar():
         while(headerReceived is False):
             while (self.uart.read(1).hex() != DATA_HEADER_LOWER):
                 pass
-            if (self.uart.read(2).hex() == DATA_HEADER_UPPER):
-                headerReceived = True
+            if (self.uart.read(1).hex() == DATA_HEADER_UPPER):
+                if(self.uart.read(1).hex() != '01'):
+                    headerReceived = True
 
         self.numVals = int(self.uart.read(1).hex(), 16)
 
-        fsaL = self.uart.read(1).hex()
-        fsaU = self.uart.read(1).hex()
-        self.fsa = int(fsaU + fsaL, 16)
+        tmpL = self.uart.read(1)
+        fsaL = tmpL[0]
         
-        lsaL = self.uart.read(1).hex()
-        lsaU = self.uart.read(1).hex()
-        self.lsa = int(lsaU + lsaL, 16)
+        tmpU = self.uart.read(1)
+        fsaU = tmpU[0]
+        
+        self.fsa = (fsaU * 256) + fsaL
+        
+        tmpL = self.uart.read(1)
+        lsaL = tmpL[0]
+
+        tmpU = self.uart.read(1)
+        lsaU = tmpU[0]
+
+        self.lsa = (lsaU * 256) + lsaL
 
         csL = self.uart.read(1).hex()
         csU = self.uart.read(1).hex()
         self.cs = int(csU + csL, 16)
 
-        self.rawData = self.uart.read(self.numVals * 2).hex()
+        self.rawData = self.uart.read(self.numVals * 2)
         self.dist = [0] * self.numVals
         self.ang =  [0] * self.numVals
 
@@ -46,19 +55,29 @@ class Lidar():
         endAng   = (self.lsa >> 1) / 64
 
         diff = endAng - startAng
-        #if diff < 0:
-        #    diff += 360
+        if diff < 0:
+            diff += 360
 
         for i in range(self.numVals):
-            distL = self.rawData[i * 4] + self.rawData[i * 4 + 1]
-            distU = self.rawData[(i * 4 ) + 2] + self.rawData[(i * 4 ) + 3]
-            self.dist[i] = int(distU + distL, 16)
+            distL = self.rawData[i * 2]
+            distU = self.rawData[i * 2 + 1]
+
+            self.dist[i] = distU * 256 + distL
 
             checksum ^= self.dist[i]
 
             self.dist[i] = self.distCorrection(self.dist[i])
             ang = self.angCorrection1(diff, startAng, i)
             self.ang[i] = self.angCorrection2(self.dist[i], ang)
+            if self.ang[i] < 0:
+                self.ang[i] = self.ang[i] + 360
+            elif self.ang[i] >= 360:
+                self.ang[i] = self.ang[i] - 360
+
+            # Error
+            if self.dist[i] == 0 or self.dist[i] == 0.5:
+                self.dist[i] = 0.0
+                self.ang[i] = 0.0
 
         checksum ^= (self.numVals << 8)
         checksum ^= (self.lsa)
@@ -69,6 +88,8 @@ class Lidar():
 
     # i: index, starting from 0
     def angCorrection1(self, angDiff, startAng, i):
+        if self.numVals == 1:
+            return startAng
         return (angDiff / (self.numVals - 1)) * (i) + startAng
 
     # dist: the corrected distance, ang: the corrected angle from angCorrection1
