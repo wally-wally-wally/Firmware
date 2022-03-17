c4ba252373e65073d20e552534004824197bbca0
Riley <42981250+rs2thoma@users.noreply.github.com>
2022-03-16 23:57:21 -0400

    Cell rebalance (#25)
    * redo cell rebalancing
    
    * updates
    
    * added  bat mon
    
    * changed lidar angles
    
    Co-authored-by: Riley <rs2thoma@uwaterloo.ca>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 import global_vars
 from system_monitor import sysMon
 from main_task import mainTask
+from battery_monitor import batMon
 
 # Shared resources 
 # Since we're accessing different pins I don't think locks are needed on GPIO functions 
@@ -18,11 +19,14 @@ if __name__ == '__main__':
   #  init()
     global_vars.init()
     sysMon = threading.Thread(target=sysMon)
+    batMon = threading.Thread(target=batMon)
     mainTask = threading.Thread(target=mainTask)
        
     sysMon.start()
     mainTask.start()
+    batMon.start()
 
     sysMon.join()
     mainTask.join()
+    batMon.join()
 
--- system_monitor.py
+++ system_monitor.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import global_vars
+#import GPIO
 #import BMS
 
 from uart import UART
@@ -11,8 +12,8 @@ import lidar
 #CollisionDetected = False
 #CollisionEvent = threading.Event()
 
-I2c = None
-Charger = None
+#I2c = None
+#Charger = None
 Cells = [None] * 4
 
 DIST_THRESH = 1000 # mm
@@ -20,7 +21,7 @@ DIST_THRESH = 1000 # mm
 # Should be defined in BMS
 NUM_CELLS = 4
 MAX_VOLT_THRESH = 4.2
-MIN_VOLT_THRESH = 4.158 # 2 * 0.99
+MIN_VOLT_THRESH = 3.2#4.158 # 2 * 0.99
 DISCHARGE_TOL = 1.01 # 1% Tolerance
 
 # Array of bools for each degree; True if an object is within DIST_THRESH
@@ -30,25 +31,31 @@ def init():
 
     global Uart
     global Lidar
+    global I2c
     Uart = UART(lidar.PORT, lidar.BAUD)
     Lidar = lidar.Lidar(Uart)
+#    I2c = i2c.I2C(1)
     global obstruction
     obstruction = False
-    print(Lidar)
+#    GPIO.init()
+#    for i in range(NUM_CELLS):
+#        GPIO.setPin(CELL_GPIO_EN[i], 'OUT', 'NONE')
+    #print(Lidar)
     #I2c = i2c.I2C(i2c.MAIN_CHANNEL)
     #Charger = BMS.Charger(I2c)
-    #for i in range(BMS.NUM_CELLS)
+    #for i in 
+    # range(BMS.NUM_CELLS)
         #Cells[i] = BMS.Cell(i, enPin[i], I2c)
 
 def getAnglesFromDirection():
     if global_vars.WallyDirection == 'B':
-        return -45, 45
+        return -40, 40
     elif global_vars.WallyDirection == 'L':
-        return 45, 135
+        return 50, 130
     elif global_vars.WallyDirection == 'F':
-        return 135, 225
+        return 140, 220
     elif global_vars.WallyDirection == 'R':
-        return 225, 315
+        return 230, 310
     elif global_vars.WallyDirection == 'N':
         return 0, 0
 
@@ -100,44 +107,43 @@ def CollisionDetection():
 # Not Currently in use/tested
 def CellRebalance():
     dischargingCells = False
-    Charger.checkChargeStatus()
         
-    if Charger.isCharging is False:
+    #if Charger.isCharging is False:
+    voltage = [0] * 4
+    for i in range(NUM_CELLS):
+        voltage[i] = Cells[i].readVoltage()
+
+    maxVoltage = max(voltage)
+    minVoltage = min(voltage)
+    minIndex = voltage.index(minVoltage)
+    #if maxVoltage >= MAX_VOLT_THRESH and minVoltage >= MIN_VOLT_THRESH:
+    #   Charger.enableDevices()
+    if (maxVoltage >= MAX_VOLT_THRESH) or (maxVoltage > minVoltage * DISCHARGE_TOL):
+        dischargingCells = True
+    #    Charger.disableCharging()
+        for i in range(NUM_CELLS):
+            if i != minIndex:
+                Cells[i].dischargeCell()
+
+    #elif Charger.isCharging is True and dischargingCells is True:
+    allCellsDischarged = True
+    while allCellsDischarged is True:
         voltage = [0] * 4
         for i in range(NUM_CELLS):
             voltage[i] = Cells[i].readVoltage()
-
-        maxVoltage = max(voltage)
-        minVoltage = min(voltage)
-        minIndex = voltage.index(minVoltage)
-        if maxVoltage >= MAX_VOLT_THRESH and minVoltage >= MIN_VOLT_THRESH:
-           Charger.enableDevices()
-        elif maxVoltage >= MAX_VOLT_THRESH:
-            dischargingCells = True
-            Charger.disableCharging()
-            for i in range(NUM_CELLS):
-                if i != minIndex:
-                    Cells[i].dischargeCell()
-
-    elif Charger.isCharging is True and dischargingCells is True:
-        allCellsDischarged = True
-        voltage = [0] * 4
-        for i in range(NUM_CELLS):
-            voltage[i] = Cells[i].readVoltage()
-            if voltage[i] <= (min(voltage) * DISCHARGE_TOL):
+            if voltage[i] <= (min(voltage) * DISCHARGE_TOL) and (voltage[i] < MAX_VOLT_THRESH):
                 Cells[i].stopDischarge()
             if Cells[i].isDischarging is True:
                 allCellsDischarged = False
-
-        if allCellsDischarged is True:
-            Charger.enableCharging()
-            dischargingCells = False
+        
+        #time.sleep(0.5)
 
 def sysMon():
     init()
 #    print('sysmon', flush=True)
     #print(Lidar)
     #print(obstruction)
+    #CellRebalance()
 
     while True:
         #time.sleep(1.5)
@@ -146,7 +152,13 @@ def sysMon():
 
         CollisionDetection()
 
+        #CellMonitor()
+
 if __name__ == '__main__':
     global_vars.init()
-    sysMon()
+    init()
+    while True:
+        print(readCellVoltages())        
+        time.sleep(1)
+    #sysMon()
 
